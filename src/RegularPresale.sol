5a42d837b3a67e0cb046c2e87408322fd02be8c1
spyros-zikos <zikossp@gmail.com>
2024-11-25 20:29:54 +0200

    I think the only thing left in the regular presale is the pool deployments

--- src/RegularPresale.sol
+++ src/RegularPresale.sol
@@ -10,13 +10,14 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 event ProjectCreated(
     uint256 lastProjectId,
     address token,
-    uint256 tokenPrice, 
-    uint256 initialTokenAmount, 
-    uint256 hardCap, 
-    uint256 startTime, 
+    uint256 tokenPrice,
+    uint256 initialTokenAmount,
+    uint256 startTime,
     uint256 endTime
 );
-event ProjectJoined(uint256 id, address contributor, uint256 tokenAmount);
+event UserJoinedProject(uint256 id, address contributor, uint256 tokenAmount);
+event UserLeftProject(uint256 id, address contributor, uint256 etherToGiveBack);
+
 
 // Create presale errors
 error InvalidTokenAddress(address token);
@@ -25,17 +26,23 @@ error StartTimeMustBeInFuture(uint256 startTime, uint256 currentTime);
 error EndTimeMustBeAfterStartTime(uint256 startTime, uint256 endTime);
 error IncorrectCreationFee(uint256 feePaid, uint256 actualFee);
 error InitialTokenAmountMustBeEven(uint256 initialTokenAmount);
-error IncorrectHardCap(uint256 hardCap, uint256 maxPresaleTokenAmount);
 // Join presale errors
 error InvalidProjectId(uint256 id);
 error ProjectIsNotPending(uint256 id);
 error ProjectHasNotStarted(uint256 id);
 error ProjectHasEnded(uint256 id);
-error HardCapSurpassed(uint256 tokenAmount, uint256 hardCap);
+error NoMoreTokensToGive(uint256 id);
+// Leave presale errors
+error ProjectHasNotFailed(uint256 id);
+error UserHasNotContributed(uint256 id, address contributor);
+// End presale errors
+error ProjectHasNotEnded(uint256 id);
+// Other
+error EtherTransferFailed(address to, uint256 value);
 
 enum ProjectStatus {
     Pending,
-    Completed,
+    Success,
     Failed
 }
 
@@ -43,24 +50,35 @@ struct Project {
     address token;
     uint256 price; // in USD, 18 decimals
     uint256 initialTokenAmount;
-    uint256 raised;
-    uint256 hardCap;
+    uint256 raised; // in ETH
     uint256 startTime;
     uint256 endTime;
+    address creator;
     address[] contributors;
     ProjectStatus status;
 }
 
 contract RegularPresale is Ownable, ReentrancyGuard {
+    uint256 constant DECIMALS = 1e18;
     uint256 private s_creationFee;
+    uint256 private s_successfulEndFee; // percentage, e.g. 10e16 = 10%
     address private s_feeCollector;
     AggregatorV3Interface private s_priceFeed;
     uint256 private s_lastProjectId; // starts from 1
     mapping (uint256 id => Project project) private s_projectFromId;
-    mapping (uint256 id => mapping(address contributor => uint256 tokenAmount)) s_contributions;
+    mapping (uint256 id => mapping(address contributor => uint256 tokenAmount)) s_tokensOwedToContributor;
 
-    constructor(uint256 _creationFee, address _feeCollector, address _priceFeed) Ownable(msg.sender) {
+    modifier validId(uint256 _id) {
+        // Check if project id is valid
+        if (_id > s_lastProjectId || _id <= 0) {
+            revert InvalidProjectId(_id);
+        }
+        _;
+    }
+
+    constructor(uint256 _creationFee, uint256 _successfulEndFee, address _feeCollector, address _priceFeed) Ownable(msg.sender) {
         s_creationFee = _creationFee;
+        s_successfulEndFee = _successfulEndFee;
         s_feeCollector = _feeCollector;
         s_priceFeed = AggregatorV3Interface(_priceFeed);
     }
@@ -72,7 +90,6 @@ contract RegularPresale is Ownable, ReentrancyGuard {
         address _token,
         uint256 _tokenPrice,
         uint256 _initialTokenAmount, // must be even so that half goes to presale and half to pool
-        uint256 _hardCap, // in tokens, has to be <= initialTokenAmount / 2
         uint256 _startTime,
         uint256 _endTime
     ) external payable nonReentrant {  // probably does not need nonReentrant but just in case
@@ -105,17 +122,11 @@ contract RegularPresale is Ownable, ReentrancyGuard {
         if (_initialTokenAmount % 2 != 0) {
             revert InitialTokenAmountMustBeEven(_initialTokenAmount);
         }
-        // Check if hard cap is correct
-        if (_hardCap > getMaxPresaleTokenAmount(_initialTokenAmount)) {
-            revert IncorrectHardCap(_hardCap, getMaxPresaleTokenAmount(_initialTokenAmount));
-        }
-
         ////////////////////////////////////////
         // Actions /////////////////////////////
         ////////////////////////////////////////
-        // Take initial token amount from user to this contract
+        // Transfer initial token amount from user to this contract
         IERC20(_token).transferFrom(msg.sender, address(this), _initialTokenAmount);
-
         s_lastProjectId += 1;
         address[] memory _contributors;
         s_projectFromId[s_lastProjectId] = Project({
@@ -123,23 +134,19 @@ contract RegularPresale is Ownable, ReentrancyGuard {
             price: _tokenPrice,
             initialTokenAmount: _initialTokenAmount,
             raised: 0,
-            hardCap: _hardCap,
             startTime: _startTime,
             endTime: _endTime,
+            creator: msg.sender,
             contributors: _contributors,
             status: ProjectStatus.Pending
         });
-        emit ProjectCreated(s_lastProjectId, _token, _tokenPrice, _initialTokenAmount, _hardCap, _startTime, _endTime);
+        emit ProjectCreated(s_lastProjectId, _token, _tokenPrice, _initialTokenAmount, _startTime, _endTime);
     }
 
-    function joinProjectPresale(uint256 _id) external payable nonReentrant {
+    function joinProjectPresale(uint256 _id) external payable nonReentrant validId(_id) {
         ////////////////////////////////////////
         // Checks //////////////////////////////
         ////////////////////////////////////////
-        // Check if project id is valid
-        if (_id > s_lastProjectId || _id <= 0) {
-            revert InvalidProjectId(_id);
-        }
         // Check if the project is pending
         if (s_projectFromId[_id].status != ProjectStatus.Pending) {
             revert ProjectIsNotPending(_id);
@@ -149,34 +156,86 @@ contract RegularPresale is Ownable, ReentrancyGuard {
             revert ProjectHasNotStarted(_id);
         }
         // Check if the project has ended
-        if (s_projectFromId[_id].endTime < block.timestamp) {
+        if (projectHasEnded(_id)) {
             revert ProjectHasEnded(_id);
         }
-        uint256 tokenAmount = (msg.value * 1e18 / s_projectFromId[_id].price);
-        // Check if contributions surpass hardcap
-        if (tokenAmount > s_projectFromId[_id].hardCap) {
-            revert HardCapSurpassed(tokenAmount, s_projectFromId[_id].hardCap);
+        // If no more tokens to give revert
+        if (getRemainingTokens(_id) == 0) {
+            revert NoMoreTokensToGive(_id);
         }
-
         ////////////////////////////////////////
         // Actions /////////////////////////////
         ////////////////////////////////////////
+        uint256 tokenAmount = msg.value * DECIMALS / s_projectFromId[_id].price;
+        // Check if contributions surpass max presale token amount, then give only what is left
+        if (IERC20(s_projectFromId[_id].token).balanceOf(address(this)) + tokenAmount > getMaxPresaleTokenAmount(_id)) {
+            tokenAmount = getRemainingTokens(_id);
+        }
         // Add contributor to project
         if (!contributorExists(_id, msg.sender)) {
             s_projectFromId[_id].contributors.push(msg.sender);
         }
-        s_contributions[_id][msg.sender] += tokenAmount;
-        // Send tokens to contributor
-        IERC20(s_projectFromId[_id].token).transfer(msg.sender, tokenAmount);
-        emit ProjectJoined(_id, msg.sender, tokenAmount);
+        s_tokensOwedToContributor[_id][msg.sender] += tokenAmount;
+        s_projectFromId[_id].raised += msg.value;
+        emit UserJoinedProject(_id, msg.sender, tokenAmount);
     }
 
-    function leaveAfterUnsuccessfulPresale() external {
-        // TODO
+    // Needs the user to approve the token transfer before calling this function
+    function leaveAfterUnsuccessfulPresale(uint256 _id) external payable nonReentrant validId(_id) {
+        // Check if the project has failed
+        if (s_projectFromId[_id].status != ProjectStatus.Failed) {
+            revert ProjectHasNotFailed(_id);
+        }
+        // Check if user has contributed
+        if (!contributorExists(_id, msg.sender)) {
+            revert UserHasNotContributed(_id, msg.sender);
+        }
+
+        // Calculate ether to give back
+        uint256 etherToGiveBack = s_tokensOwedToContributor[_id][msg.sender] * s_projectFromId[_id].price / DECIMALS;
+        // Reset tokens owed to user
+        s_tokensOwedToContributor[_id][msg.sender] = 0;
+        // give it back
+        sendEther(payable(msg.sender), etherToGiveBack);
+        emit UserLeftProject(_id, msg.sender, etherToGiveBack);
     }
 
-    function endPresale() external {
-        // TODO
+    function endPresale(uint256 _id) external nonReentrant validId(_id) {
+        // Check if the project is pending
+        if (s_projectFromId[_id].status != ProjectStatus.Pending) {
+            revert ProjectIsNotPending(_id);
+        }
+
+        if (projectHasEnded(_id)) {
+            if (projectSuccessful(_id)) {
+                s_projectFromId[_id].status = ProjectStatus.Success;
+            } else {
+                s_projectFromId[_id].status = ProjectStatus.Failed;
+            }
+        } else {
+            revert ProjectHasNotEnded(_id);
+        }
+
+        if (projectSuccessful(_id)) {
+            
+            // TODO: distribute tokens to contributors
+            for (uint256 i = 0; i < s_projectFromId[_id].contributors.length; i++) {
+                address contributor = s_projectFromId[_id].contributors[i];
+                uint256 tokensToGive = s_tokensOwedToContributor[_id][contributor];
+                IERC20(s_projectFromId[_id].token).transfer(contributor, tokensToGive);
+                s_tokensOwedToContributor[_id][contributor] = 0; // that's probably not needed
+            }
+            // Calculate successful-end fee ether amount
+            uint256 successfulEndFeeAmount = s_projectFromId[_id].raised * s_successfulEndFee / 1e18;
+            // Send ether as fee to project creator
+            sendEther(payable(s_projectFromId[_id].creator), successfulEndFeeAmount);
+            // Reduce amount raised by 2*successfulEndFeeAmount
+            // so that successfulEndFeeAmount is sent to creator
+            // and successfulEndFeeAmount remains in the contract for the fee collector to collct
+            uint256 amountRaisedAfterFees = s_projectFromId[_id].raised - (2 * successfulEndFeeAmount);
+            // TODO: send tokens to pool
+
+        }
     }
 
     function withdrawFunds() external onlyOwner {
@@ -184,8 +243,30 @@ contract RegularPresale is Ownable, ReentrancyGuard {
         payable(s_feeCollector).transfer(address(this).balance);
     }
 
-    function getSoftCap(uint256 _id) external view returns (uint256) {
-        return s_projectFromId[_id].hardCap * 3 / 10;
+    ////////////////////////////////////////
+    // Helpers /////////////////////////////
+    ////////////////////////////////////////
+
+    function getSoftCap(uint256 _id) public view returns (uint256) {
+        return getMaxPresaleTokenAmount(s_projectFromId[_id].initialTokenAmount) * 3 / 10;
+    }
+
+    function getTotalTokensOwed(uint256 _id) public view returns (uint256) {
+        uint256 totalTokensOwed = 0;
+        for (uint256 i = 0; i < s_projectFromId[_id].contributors.length; i++) {
+            totalTokensOwed += s_tokensOwedToContributor[_id][s_projectFromId[_id].contributors[i]];
+        }
+        return totalTokensOwed;
+    }
+
+    function getRemainingTokens(uint256 _id) public view returns (uint256) {
+        // tokens that have been distributed
+        uint256 distributedTokens = s_projectFromId[_id].initialTokenAmount - IERC20(s_projectFromId[_id].token).balanceOf(address(this));
+        // max tokens that can be distributed
+        uint256 maxTokensToBeDistributed = getMaxPresaleTokenAmount(_id);
+        // tokens that can be distributed
+        uint256 remainingTokens = maxTokensToBeDistributed - distributedTokens;
+        return remainingTokens;
     }
 
     function contributorExists(uint256 _id, address _contributor) public view returns (bool) {
@@ -197,9 +278,27 @@ contract RegularPresale is Ownable, ReentrancyGuard {
         return false;
     }
 
-    function getMaxPresaleTokenAmount(uint256 _initialTokenAmount) public pure returns (uint256) {
-        return _initialTokenAmount / 2;
+    function getMaxPresaleTokenAmount(uint256 _id) public view returns (uint256) {
+        return s_projectFromId[_id].initialTokenAmount / 2;
+    }
+
+    function sendEther(address payable _to, uint256 _value) private {
+        // Call returns a boolean value indicating success or failure.
+        (bool sent,) = _to.call{value: _value}("");
+        if (!sent) {
+            revert EtherTransferFailed(_to, _value);
+        }
+    }
+
+    function projectHasEnded(uint256 _id) public view returns (bool) {
+        return s_projectFromId[_id].endTime < block.timestamp;
+    }
+
+    function projectSuccessful(uint256 _id) public view returns (bool) {
+        return getTotalTokensOwed(_id) >= getSoftCap(_id);
     }
 
     // TODO get stats functions
+
+    // Future: check fees in range
 }
